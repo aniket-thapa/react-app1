9232778852a116736ad99be5e31c6933364e28c6
Aniket Thapa <aniketthapa04@gmail.com>
2025-02-24 23:32:44 +0530

    get Classy

---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+const User = () => {
+  return (
+    <div>
+      <h2>Name: Aniket Thapa</h2>
+      <h3>Location: Jammu, J&K</h3>
+      <h4>Email Id: aniketthapa.dev@gmail.com</h4>
+    </div>
+  );
+};
+
+export default User;
