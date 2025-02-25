2d695b2a7a999124929c42f8213745e8f907727d
Aniket Thapa <aniketthapa04@gmail.com>
2025-02-26 02:00:27 +0530

    fixed

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -8,7 +8,7 @@ const RestaurantCard = (props) => {
     <div className="m-2 p-2 w-72 flex flex-col items-center border-2 border-gray-300 hover:border-gray-400 hover:shadow-lg rounded-lg transition">
       <img className="w-56" alt="res-loog" src={LOGO_URL} />
       <h3 className="py-2 font-semibold text-gray-700 text-2xl ">{name}</h3>
-      <p className="text-lg">{cuisines}</p>
+      <p className="text-lg">{cuisines.join(', ')}</p>
       <p className="text-lg font-medium">Rs. {cost}</p>
       <p className="text-lg">⭐{star_ratings} stars</p>
       <p className="text-lg">{delivery_time}</p>
