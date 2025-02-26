8efbd84977573c6cc63ee083b39ed7cd3b7268dd
Aniket Thapa <aniketthapa04@gmail.com>
2025-02-26 16:20:17 +0530

    fixed

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,7 +4,7 @@ const ItemList = ({ items }) => {
       {items.map((item) => (
         <div
           key={item.card.info.id}
-          className="p-2 m-2 border-gray-300 border-b-2"
+          className="p-2 pb-8 m-2 border-gray-300 border-b-2 flex flex-col justify-center items-center"
         >
           <div className="p-2 font-semibold text-lg text-gray-700">
             <span>{item.card.info.name}</span>
@@ -16,6 +16,9 @@ const ItemList = ({ items }) => {
             </span>
           </div>
           <p className="text-md text-gray-700">{item.card.info.description}</p>
+          <button className="self-end w-fit px-2 py-1 text-center font-semibold bg-blue-200 border-2 rounded-md border-gray-600 hover:bg-blue-300  cursor-pointer transition">
+            Add
+          </button>
         </div>
       ))}
     </div>
